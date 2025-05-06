130a40774df1ac511a59481697c5cc6cb6f0935d
Joshua Hursey <jhursey@us.ibm.com>
2025-05-06 15:08:20 -0400

    Options for Stagger model loading for low memory systems
     * `--stagger_load` : (default: `0` off) Stagger model loading to avoid OOM issues on the host
     * `--stagger_update_lazyhandle` : (default: `0` off) Stagger update_lazyhandle to avoid OOM issues on the host
     * `--dist_timeout` : (default: either `10` for NCCL or `30` for others set by PyTorch) torch distributed timeout in minutes
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- aiu_fms_testing_utils/utils/__init__.py
+++ aiu_fms_testing_utils/utils/__init__.py
@@ -3,25 +3,61 @@ import torch.nn as nn
 import time
 from fms.utils.tokenizers import BaseTokenizer
 from fms.utils.generation import generate
-from aiu_fms_testing_utils.utils.aiu_setup import dprint
+from aiu_fms_testing_utils.utils.aiu_setup import dprint, rank, local_rank, world_size
 from typing import Optional, List, Tuple
 import os
 import requests
 import json
 import random
+import math
 
-def warmup_model(model: nn.Module, input_ids: torch.Tensor, max_new_tokens: int, compile_dynamic_sendnn = False, **padding_kwargs):
+def stagger_enter(limit: int):
+    """
+    Limit the number of concurrent processes into this section of code.
+    Processes return from this funciton when they are allowed to enter the section of code.
+    Must be paired with another call to stagger_leave() when exiting the section of code.
+
+    :param limit: Number of concurrent processes allowed in the code section if > 0.
+    """
+    if limit > 0 and limit != world_size:
+        for _set in range( math.ceil(world_size / float(limit)) ):
+            if rank < (_set+1)*limit:
+                break
+            torch.distributed.barrier()
+        dprint(f"Stagger: Enter (Set: {_set+1} of {math.ceil(world_size / float(limit))})")
+
+def stagger_leave(limit: int):
+    """
+    Leave a section code where the number of concurrent processes is limited.
+    Processes return from this funciton when all of the processes have completed the section of code.
+    Must be paired with another call to stagger_enter() when entering the section of code.
+
+    :param limit: Number of concurrent processes allowed in the code section if > 0.
+    """
+    if limit > 0 and limit != world_size:
+        for _set in range( math.ceil(world_size / float(limit)) ):
+            if rank >= (_set+1)*limit:
+                continue
+            torch.distributed.barrier()
+        dprint(f"Stagger: All Complete")
+
+def warmup_model(model: nn.Module, input_ids: torch.Tensor, max_new_tokens: int, compile_dynamic_sendnn = False, stagger_update_lazyhandle = 0, **padding_kwargs):
     import torch_sendnn
     dprint("AIU warmup")
-    pt_compile_model_time = time.time()
     extra_kwargs = {**padding_kwargs, "only_last_token": True}
     max_new_tokens_warmup = max_new_tokens
     if compile_dynamic_sendnn:
         max_new_tokens_warmup = 2
+
+    stagger_enter(stagger_update_lazyhandle)
+
+    pt_compile_model_time = time.time()
     with torch_sendnn.warmup_mode():
         generate(model, input_ids, max_new_tokens=max_new_tokens_warmup, max_seq_len=model.config.max_expected_seq_len, use_cache=True, do_sample=False, contiguous_cache=True, extra_kwargs=extra_kwargs)
     pt_compile_model_time = time.time() - pt_compile_model_time
     dprint(f"PT compile complete, took {pt_compile_model_time:.3f}s")
+
+    stagger_leave(stagger_update_lazyhandle)
 
 def ids_for_prompt(prompt, tokenizer):
     tokens = tokenizer.tokenize(prompt)
