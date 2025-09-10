c8016a96823648759793772cb7c59f4246e41555
Will Johnson <mwjohnson728@gmail.com>
2025-09-10 13:45:41 -0400

    bf16
    Signed-off-by: Will Johnson <mwjohnson728@gmail.com>

--- tests/models/test_decoders.py
+++ tests/models/test_decoders.py
@@ -56,7 +56,7 @@ micro_model_mapping = {
     ),
     # FIXME: Because this uses the same config as 3.2, re-using here, but should update
     GRANITE_3p3_8B_INSTRUCT: os.path.join(
-        MICRO_MODELS_HOME, "granite-3.3-8b-layers-4-step-100000"
+        MICRO_MODELS_HOME, "granite-3.3-8b-layers-4-step-100000-bf16"
     ),
     LLAMA_3p1_70B_INSTRUCT: os.path.join(
         MICRO_MODELS_HOME, "llama-3.1-70b-layers-3-step-24000"
